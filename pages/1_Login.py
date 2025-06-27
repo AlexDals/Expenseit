59999512894fb0ddb0b0680f53c0208e86cb97d5
AlexDals <100142352+AlexDals@users.noreply.github.com>
2025-06-27 09:41:49 -0400

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from utils.ui_utils import hide_streamlit_pages_nav
+
+# *First thing* on the page:
+hide_streamlit_pages_nav()
+
+st.set_page_config(page_title="Login", layout="wide")
 
 st.title("Employee Expense Reporting")
 st.write("Please log in to access your dashboard.")
